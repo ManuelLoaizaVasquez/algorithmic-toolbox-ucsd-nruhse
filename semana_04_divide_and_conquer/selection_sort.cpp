bab6e55122c468e7b401116e6cb5c782a7b9509d
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-08-04 14:02:43 -0500

    O(n^2)

---
+++ semana_04_divide_and_conquer/selection_sort.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int a[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (a[j] < a[min_index]) {
+                min_index = j;
+            }
+        }
+        swap(a[i], a[min_index]);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    selection_sort(a, n);
+    cout << "Arreglo ordenado : " << endl;
+    for (int i = 0; i < n; i++) {
+        if (i > 0) cout << " ";
+        cout << a[i];
+    }
+    cout << endl;
+    return 0;
+}
